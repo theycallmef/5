c93620f8d5e9011067ff2c87d5a4968225251325
Feruza <252208@astanait.edu.kz>
2026-01-29 01:44:17 +0600

    init commit

---
+++ DatabaseConnection.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/freelance_db";
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "zarina";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ JobListingDAO.java
@@ -0,0 +1,61 @@
+import java.sql.*;
+
+public class JobListingDAO {
+
+    // CREATE (write)
+    public void addJob(JobListing job) throws SQLException {
+        String sql =
+                "INSERT INTO job_listing (title, description, budget) VALUES (?, ?, ?)";
+
+        Connection conn = DatabaseConnection.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+
+        stmt.setString(1, job.getTitle());
+        stmt.setString(2, job.getDescription());
+        stmt.setDouble(3, job.getBudget());
+
+        stmt.executeUpdate();
+    }
+
+    // READ
+    public void getAllJobs() throws SQLException {
+        String sql = "SELECT * FROM job_listing";
+
+        Connection conn = DatabaseConnection.getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while (rs.next()) {
+            System.out.println(
+                    rs.getInt("id") + " | " +
+                            rs.getString("title") + " | " +
+                            rs.getDouble("budget")
+            );
+        }
+    }
+
+    // UPDATE
+    public void updateBudget(int id, double newBudget) throws SQLException {
+        String sql =
+                "UPDATE job_listing SET budget = ? WHERE id = ?";
+
+        Connection conn = DatabaseConnection.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+
+        stmt.setDouble(1, newBudget);
+        stmt.setInt(2, id);
+
+        stmt.executeUpdate();
+    }
+
+    // DELETE
+    public void deleteJob(int id) throws SQLException {
+        String sql = "DELETE FROM job_listing WHERE id = ?";
+
+        Connection conn = DatabaseConnection.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+
+        stmt.setInt(1, id);
+        stmt.executeUpdate();
+    }
+}
